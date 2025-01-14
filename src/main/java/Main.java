a3d27896f5a33d1a47a1e7c61c1a121f64053f28
Raghavv-gupta <raghavguptaji0@gmail.com>
2025-01-14 14:32:08 +0530

    implemented the echo builtin...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        while(true){
+        while (true) {
 
             System.out.print("$ ");
-    
+
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-    
-            if(input.startsWith("exit")){
+
+            if (input.startsWith("exit")) {
                 int exitCode = Integer.parseInt(input.substring(5));
 
                 System.exit(exitCode);
-            }else{
+            } else if (input.startsWith("echo")) {
+                String output = input.substring(5);
+                System.out.println(output);
+            } else {
 
                 System.out.println(input + ": command not found");
             }
