c9adf8408c29e4ed85fa16329c9bccfd855eb5f0
Raghavv-gupta <raghavguptaji0@gmail.com>
2025-01-14 19:21:59 +0530

    implemented the type builtin command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+        String input, typeSubstring;
+        String[] commands = {"echo", "exit", "type"};
         while (true) {
 
             System.out.print("$ ");
@@ -17,6 +20,13 @@ public class Main {
             } else if (input.startsWith("echo")) {
                 String output = input.substring(5);
                 System.out.println(output);
+            }else if (input.startsWith("type")){
+                typeSubstring = input.substring(5);
+                if(Arrays.asList(commands).contains(typeSubstring)){
+                    System.out.println(typeSubstring + " is a shell builtin");
+                }else {
+                    System.out.println(typeSubstring + " not found");
+                }
             } else {
 
                 System.out.println(input + ": command not found");
